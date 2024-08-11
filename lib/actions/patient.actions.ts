4dacde687f92db1e1a88b5e54fcb33584559c667
koolmind <simoneconti.dev@gmail.com>
2024-08-11 12:52:39 +0200

    create user issue commit

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,28 @@
+"use server";
+
+import { ID, Query } from "node-appwrite";
+import { users } from "../appwrite.config";
+import { parseStringify } from "../utils";
+
+// CREATE APPWRITE USER
+export const createUser = async (user: CreateUserParams) => {
+  try {
+    // Create new user -> https://appwrite.io/docs/references/1.5.x/server-nodejs/users#create
+    const newUser = await users.create(
+      ID.unique(),
+      user.email,
+      user.phone,
+      undefined, // password is not needed
+      user.name
+    );
+    return parseStringify(newUser);
+  } catch (error: any) {
+    if (error && error?.code === 409) {
+      // utente già presente nel DB. Lo recupero e lo ritorno
+      const documents = await users.list([Query.equal("email", [user.email])]);
+
+      return documents?.users[0];
+    }
+    console.error("An error occurred while creating a new user:", error);
+  }
+};
